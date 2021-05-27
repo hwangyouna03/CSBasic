d48ddb9b6531409b63992c6b83de0f8a92db9081
Yuna Hwang <hwangyouna04@daum.net>
2021-05-28 08:31:05 +0900

    클래스와 인스턴스 실습

--- CSBasic4/Program.cs
+++ CSBasic4/Program.cs
@@ -6,10 +6,94 @@ using System.Threading.Tasks;
 
 namespace CSBasic4
 {
+    class MyMath
+    {
+        int somenumber = 42;
+        public static double PI = 3.141592;
+        public static void Greeting()
+        {
+            //Console.WriteLine(somenumber);
+            Console.WriteLine("Greeting!");
+        }
+    }
+
+    /*class Math
+    {
+    }*/
+
+    class Car
+    {
+        int carNumber;
+        DateTime inTime;
+        DateTime outTime;
+
+        public void SetInTime()
+        {
+            this.inTime = DateTime.Now;
+        }
+        public void SetOutTime()
+        {
+            this.inTime = DateTime.Now;
+        }
+    }
     class Program
     {
+        class FirstClass
+        {
+
+        }
+
+        class SecondClass
+        {
+
+        }
         static void Main(string[] args)
         {
+            Car[] cars = new Car[10];
+
+            Random random = new Random();
+            Console.WriteLine(random.Next());
+            Console.WriteLine(random.Next(100));
+            Console.WriteLine(random.Next(1, 19));
+            Console.WriteLine(random.NextDouble());
+            Console.WriteLine(random.NextDouble());
+            Console.WriteLine(random.NextDouble());
+            Console.WriteLine(random.NextDouble());
+            Console.WriteLine(random.NextDouble());
+
+            //List<int> list = new List<int>();
+            //list.Add(52);
+            //list.Add(273);
+            //list.Add(32);
+            //list.Add(64);
+            List<int> list = new List<int>() { 52, 273, 32, 64 };
+
+            foreach (var item in list)
+            {
+                Console.WriteLine("Count: " + list.Count + "\titem: " + item);
+                /*list.Remove(item);*/
+            }
+
+            Console.WriteLine(Math.Abs(-52273));
+            Console.WriteLine(Math.Ceiling(52.273));
+            Console.WriteLine(Math.Floor(52.273));
+            Console.WriteLine(Math.Round(52.263));
+            Console.WriteLine(Math.Round(52.563));
+            Console.WriteLine(Math.Max(52, 273));
+            Console.WriteLine(Math.Min(52, 274));
+
+            Product product = new Product();
+            product.name = "감자";
+            product.price = 2000;
+
+            Console.WriteLine(product.name + " : " + product.price + "원");
+
+            Product productA = new Product() { name = "짜장면", price = 5500 };
+            Product productB = new Product() { name = "짬뽕", price = 5500 };
+            Product productC = new Product() { name = "탕수육" };
+            Product productD = new Product() { price = 99999999 };
+            Product productE = new Product() { price = 30000, name = "양장피" };
         }
+
     }
 }
