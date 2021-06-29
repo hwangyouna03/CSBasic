f22bbd486a770fbe1226b25a2026fb09199eb25d
Yuna Hwang <hwangyouna04@daum.net>
2021-06-30 07:51:17 +0900

    #22 클래스 기본 사용법

--- CSBasic51/Program.cs
+++ CSBasic51/Program.cs
@@ -10,6 +10,31 @@ namespace CSBasic51
     {
         static void Main(string[] args)
         {
+            Car car = new Car();
+            car.SetIntime();
+            car.setOutTime();
+
+            Random random = new Random();
+            random.Next();
+            Console.WriteLine(random.Next(10, 100));
+            Console.WriteLine(random.Next(10, 100));
+            Console.WriteLine(random.Next(20, 100));
+            Console.WriteLine(random.Next(10, 100));
+            Console.WriteLine(random.Next(10, 100));
+
+
+            Console.WriteLine(random.NextDouble()* 10);
+            Console.WriteLine(random.NextDouble()* 10);
+
+            Console.WriteLine(random.Next(20));
+            Console.WriteLine(random.Next(20)+1);
+            Console.WriteLine(random.Next(1,21));
+
+            int[] intArray = new int[10];
+            long[] longArray = new long[10];
+            string[] stringArray = new string[10];
+
+            List<int> list = new List<int>();
         }
     }
 }
