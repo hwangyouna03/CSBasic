ef2d1811baa3abd5444b410e16396ba3666ecba1
Yuna Hwang <hwangyouna04@daum.net>
2021-06-30 07:56:11 +0900

    #23 메서드 기본

--- CSBasic51/Program.cs
+++ CSBasic51/Program.cs
@@ -8,6 +8,21 @@ namespace CSBasic51
 {
     class Program
     {
+        class MethodExample
+        {
+            public int Power(int x)
+            {
+                return x * x;
+            }
+            public int Multi(int x, int y)
+            {
+                return x * y;
+            }
+            public void Print()
+            {
+                Console.WriteLine("Print() 메서드 호출됨");
+            }
+        }
         static void Main(string[] args)
         {
             Car car = new Car();
@@ -35,6 +50,13 @@ namespace CSBasic51
             string[] stringArray = new string[10];
 
             List<int> list = new List<int>();
+
+
+            MethodExample me = new MethodExample();
+            Console.WriteLine(me.Power(10));
+            Console.WriteLine(me.Power(20));
+            Console.WriteLine(me.Multi(52, 273));
+            Console.WriteLine(me.Multi(103, 32));
         }
     }
 }
