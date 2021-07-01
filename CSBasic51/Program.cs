f05006ea68513dcb253f4dea23313e2f4e5301ad
Yuna Hwang <hwangyouna04@daum.net>
2021-07-01 10:09:05 +0900

    #24 생성자 예제

--- CSBasic51/Program.cs
+++ CSBasic51/Program.cs
@@ -6,6 +6,35 @@ using System.Threading.Tasks;
 
 namespace CSBasic51
 {
+    class Product
+    {
+        public static int counter = 0;
+        public int id;
+        public string name;
+        public int price;
+
+        public override string ToString()
+        {
+            return id + ":" + name + "/" + price + "원";
+        }
+        public Product(string name, int price)
+        {
+            //Product.counter = counter + 1; //counter ++;랑 같음
+            counter++;
+            this.id = counter;
+            this.name = name;
+            this.price = price;
+        }
+    }
+    class Sample
+    {
+        public static int value;
+        static Sample()
+        {
+            value = 10;
+            Console.WriteLine("정적 생성자 호출");
+        }
+    }
     class Program
     {
         int someModifier = 0;
@@ -126,6 +155,21 @@ namespace CSBasic51
             Console.WriteLine(MyMath.Abs(-273));
             Console.WriteLine(MyMath.Abs(52.273));
             Console.WriteLine(MyMath.Abs(214712340385872)); //마우스 오버해서 타입 확인하기
+
+            Product productA = new Product("감자", 2000);
+            Product productB = new Product("고구마", 3000);
+            Product productC = new Product("옥수수", 2000);
+            Console.WriteLine(productA);
+            Console.WriteLine(productB);
+            Console.WriteLine(productC);
+            Console.WriteLine(Product.counter + "개 생성되었습니다.");
+            //Console.WriteLine(productA.id + ":" + productA.name);//toString으로 하면 이렇게 할 필요 없음
+
+            Console.WriteLine("첫 번째 위치");
+            Console.WriteLine(Sample.value);
+            Console.WriteLine("두 번째 위치");
+            Sample sample = new Sample();
+            Console.WriteLine("세 번째 위치");
 
         }
     }
