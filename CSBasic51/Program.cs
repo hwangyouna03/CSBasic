a24f8f18bda552b0d4eb70c5d36be291ebae660d
Yuna Hwang <hwangyouna04@daum.net>
2021-07-01 09:22:22 +0900

    #23 클래스 메서드 실습

--- CSBasic51/Program.cs
+++ CSBasic51/Program.cs
@@ -8,6 +8,27 @@ namespace CSBasic51
 {
     class Program
     {
+        public int instanceVariable = 10;
+        public int instanceMethod()
+        {
+            return 0;
+        }
+        class MyMath
+        {
+            public static int Abs(int input)//첫글자 대문자 Abs
+            {
+                return (input < 0) ? -input : input;
+                /*
+                if(input < 0)
+                {
+                    return -input;
+                }
+                else
+                {
+                    return input;
+                }*/
+            }
+        }
         class Test
         {
             //Method Signature : 메소드 구분할 때 쓴다.
@@ -73,6 +94,19 @@ namespace CSBasic51
 
             Test test = new Test();
             Console.WriteLine(test.Sum(1, 100)); //1부터 100까지 합
+            
+            Console.WriteLine(MyMath.Abs(52));
+            Console.WriteLine(MyMath.Abs(-273));
+
+            //클래스 메서드에서는 인스턴스 변수/메서드 접근 불가
+            //객체를 만들어 접근해야한다.
+            //Console.WriteLine(instanceVariable);
+            //instanceMethod();
+            Program p = new Program();
+            Console.WriteLine(p.instanceVariable);
+            p.instanceMethod();
+
+
         }
     }
 }
